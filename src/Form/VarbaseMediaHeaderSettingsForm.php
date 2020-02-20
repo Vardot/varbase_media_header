9e6ea09cc63515bbee5208064671e87831c2e426
Rajab Natshah <rajabn@gmail.com>
2020-02-20 19:53:02 +0200

    Issue #3110031: Drupal 9 Readiness for [Varbase Media Header] with Drupal coding standard and practice

--- src/Form/VarbaseMediaHeaderSettingsForm.php
+++ src/Form/VarbaseMediaHeaderSettingsForm.php
@@ -5,11 +5,71 @@ namespace Drupal\varbase_media_header\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\Yaml\Yaml;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
  * VarbaseMediaHeaderSettingsForm Class.
  */
 class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
+
+  /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Contains the configuration object factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a new Varbase Media Header Block.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(array $configuration, string $plugin_id, array $plugin_definition, ModuleHandlerInterface $module_handler, ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->moduleHandler = $module_handler;
+    $this->configFactory = $config_factory;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('module_handler'),
+      $container->get('config.factory'),
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -24,17 +84,20 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('varbase_media_header.settings');
 
-    $entity_info = \Drupal::entityManager()->getDefinitions();
-    $vmh_settings = $config->get(VARBASE_MEDIA_HEADER_SETTINGS);
-    $allowed_entity_types = unserialize(VARBASE_MEDIA_HEADER_ENTITY_TYPES);
+    $entity_info = $this->entityTypeManager->getStorage('node_type')->loadMultiple();
+    $vmh_settings = $config->get('varbase_media_header_settings');
 
-    $form[VARBASE_MEDIA_HEADER_SETTINGS] = [
+    // Planed for allowed entity types to have node, taxonomy.
+    // more other entity types if needed.
+    $allowed_entity_types = ['node'];
+
+    $form['varbase_media_header_settings'] = [
       '#type' => 'fieldset',
-      '#title' => t('Select entity types which are going to use the varbase media header'),
+      '#title' => $this->t('Select entity types which are going to use the varbase media header'),
       '#collapsible' => TRUE,
       '#collapsed' => FALSE,
       '#tree' => TRUE,
-      '#description' => t('Enable varbase media header for these entity types and bundles.'),
+      '#description' => $this->t('Enable varbase media header for these entity types and bundles.'),
     ];
 
     foreach ($entity_info as $entity_type_key => $entity_type) {
@@ -42,12 +105,12 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
 
       // Skip not allowed entity types.
       if (in_array($entity_type_key, $allowed_entity_types)) {
-        $bundles = \Drupal::entityManager()->getBundleInfo($entity_type_key);
+        $bundles = $this->entityTypeManager->getBundleInfo($entity_type_key);
         foreach ($bundles as $bundle_key => $bundle) {
           $bundle_options[$bundle_key] = $bundle['label'];
         }
 
-        $form[VARBASE_MEDIA_HEADER_SETTINGS][$entity_type_key] = [
+        $form['varbase_media_header_settings'][$entity_type_key] = [
           '#type' => 'checkboxes',
           '#title' => $entity_type->getLabel(),
           '#options' => $bundle_options,
@@ -66,7 +129,7 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('varbase_media_header.settings')
-      ->set(VARBASE_MEDIA_HEADER_SETTINGS, $form_state->getValue(VARBASE_MEDIA_HEADER_SETTINGS))
+      ->set('varbase_media_header_settings', $form_state->getValue('varbase_media_header_settings'))
       ->save();
 
     $this->applyDefaultVarbaseMediaHeaderSettingsForActivatedEntityTypes();
@@ -85,25 +148,25 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
    * Apply Default Varbase Media Header Settings For Activated Entity Types.
    */
   public function applyDefaultVarbaseMediaHeaderSettingsForActivatedEntityTypes() {
-    $vmh_settings = \Drupal::config('varbase_media_header.settings')->get(VARBASE_MEDIA_HEADER_SETTINGS);
-    $entity_info = \Drupal::entityManager()->getDefinitions();
+    $vmh_settings = $this->config('varbase_media_header.settings')->get('varbase_media_header_settings');
+    $entity_info = $this->entityTypeManager->getDefinitions();
 
-    $module_path = \Drupal::service('module_handler')->getModule('varbase_media_header')->getPath();
+    $module_path = $this->moduleHandler->getModule('varbase_media_header')->getPath();
 
     foreach ($entity_info as $entity_type_key => $entity_type) {
 
       // Entity Type Assets Path.
-      $entity_type_assets_path = $module_path . '/' . VARBASE_MEDIA_HEADER_ASSETS . '/' . $entity_type_key;
+      $entity_type_assets_path = $module_path . '/src/assets/config_templates/' . $entity_type_key;
 
-      $bundles = \Drupal::entityManager()->getBundleInfo($entity_type_key);
+      $bundles = $this->entityTypeManager->getBundleInfo($entity_type_key);
       foreach ($bundles as $bundle_key => $bundle) {
         if (!empty($vmh_settings[$entity_type_key])
          && isset($vmh_settings[$entity_type_key][$bundle_key])
          && $vmh_settings[$entity_type_key][$bundle_key]) {
 
           $config_name = "field.field." . $entity_type_key . "." . $bundle_key . ".field_page_header_style";
-          if (!(\Drupal::configFactory()->get($config_name) == NULL)) {
-            $config_factory = \Drupal::configFactory()->getEditable($config_name);
+          if (!($this->configFactory->get($config_name) == NULL)) {
+            $config_factory = $this->configFactory->getEditable($config_name);
             $config_entity_type_key_token = "TOKEN_" . strtoupper($entity_type_key);
             $config_template_file_name = "field.field." . $entity_type_key . "." . $config_entity_type_key_token . ".field_page_header_style.yml";
             $config_path = $entity_type_assets_path . '/' . $config_template_file_name;
@@ -117,8 +180,8 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
           }
 
           $config_name = "field.field." . $entity_type_key . "." . $bundle_key . ".field_media";
-          if (!(\Drupal::configFactory()->get($config_name) == NULL)) {
-            $config_factory = \Drupal::configFactory()->getEditable($config_name);
+          if (!($this->configFactory->get($config_name) == NULL)) {
+            $config_factory = $this->configFactory->getEditable($config_name);
             $config_entity_type_key_token = "TOKEN_" . strtoupper($entity_type_key);
             $config_template_file_name = "field.field." . $entity_type_key . "." . $config_entity_type_key_token . ".field_media.yml";
             $config_path = $entity_type_assets_path . '/' . $config_template_file_name;
@@ -132,8 +195,8 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
           }
 
           $config_name = "core.entity_form_display." . $entity_type_key . "." . $bundle_key . ".default";
-          if (!(\Drupal::configFactory()->get($config_name) == NULL)) {
-            $config_factory = \Drupal::configFactory()->getEditable($config_name);
+          if (!($this->configFactory->get($config_name) == NULL)) {
+            $config_factory = $this->configFactory->getEditable($config_name);
             $config_entity_type_key_token = "TOKEN_" . strtoupper($entity_type_key);
             $config_template_file_name = "core.entity_form_display." . $entity_type_key . "." . $config_entity_type_key_token . ".default.yml";
             $config_path = $entity_type_assets_path . '/' . $config_template_file_name;
@@ -153,8 +216,8 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
           }
 
           $config_name = "core.entity_view_display." . $entity_type_key . "." . $bundle_key . ".default";
-          if (!(\Drupal::configFactory()->get($config_name) == NULL)) {
-            $config_factory = \Drupal::configFactory()->getEditable($config_name);
+          if (!($this->configFactory->get($config_name) == NULL)) {
+            $config_factory = $this->configFactory->getEditable($config_name);
             $config_entity_type_key_token = "TOKEN_" . strtoupper($entity_type_key);
             $config_template_file_name = "core.entity_view_display." . $entity_type_key . "." . $config_entity_type_key_token . ".default.yml";
             $config_path = $entity_type_assets_path . '/' . $config_template_file_name;
--- src/Plugin/Block/VarbaseMediaHeaderBlock.php
+++ src/Plugin/Block/VarbaseMediaHeaderBlock.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\varbase_media_header\Plugin\Block;
 
-use Drupal\node\Entity\Node;
 use Drupal\node\NodeInterface;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\BlockPluginInterface;
@@ -10,6 +9,14 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Cache\Cache;
 use Drupal\media\Entity\Media;
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Controller\TitleResolverInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
  * Provides a Varbase Media Header block.
@@ -29,7 +36,127 @@ use Drupal\Core\Language\LanguageInterface;
  *   }
  * )
  */
-class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface {
+class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface, ContainerInjectionInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * Contains the configuration object factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The language manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * The title resolver.
+   *
+   * @var \Drupal\Core\Controller\TitleResolverInterface
+   */
+  protected $titleResolver;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The entity display repository.
+   *
+   * @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface
+   */
+  protected $entityDisplayRepository;
+
+  /**
+   * Constructs a new Varbase Media Header Block.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager service.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   * @param \Drupal\Core\Controller\TitleResolverInterface $title_resolver
+   *   The title resolver service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The redirect destination service.
+   * @param \Drupal\Core\Block\BlockManagerInterface $block_manager
+   *   The block manager.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   The entity field manager.
+   * @param \Drupal\Core\Entity\EntityDisplayRepositoryInterface $entity_display_repository
+   *   The entity display repository.
+   */
+  public function __construct(array $configuration, string $plugin_id, array $plugin_definition, ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager, LanguageManagerInterface $language_manager, RouteMatchInterface $route_match, TitleResolverInterface $title_resolver, RequestStack $request_stack, AccountInterface $current_user, BlockManagerInterface $block_manager, RendererInterface $renderer, EntityFieldManagerInterface $entity_field_manager, EntityDisplayRepositoryInterface $entity_display_repository = NULL) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->languageManager = $language_manager;
+    $this->routeMatch = $route_match;
+    $this->titleResolver = $title_resolver;
+    $this->requestStack = $request_stack;
+    $this->currentUser = $current_user;
+    $this->blockManager = $block_manager;
+    $this->renderer = $renderer;
+    $this->entityFieldManager = $entity_field_manager;
+    $this->entityDisplayRepository = $entity_display_repository;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory'),
+      $container->get('entity_type.manager'),
+      $container->get('language_manager'),
+      $container->get('current_route_match'),
+      $container->get('title_resolver'),
+      $container->get('request_stack'),
+      $container->get('current_user'),
+      $container->get('plugin.manager.block'),
+      $container->get('renderer'),
+      $container->get('entity_field.manager'),
+      $container->get('entity_display.repository')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -37,11 +164,11 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
   public function build() {
     $config = $this->getConfiguration();
 
-    $node = \Drupal::routeMatch()->getParameter('node');
+    $node = $this->routeMatch->getParameter('node');
     if ($node instanceof NodeInterface) {
       if (isset($node)) {
 
-        $node = Node::load($node->id());
+        $node = $this->entityTypeManager->getStorage('node')->load($node->id());
 
         if (isset($config['vmh_node'][$node->bundle()])
           && $config['vmh_node'][$node->bundle()] != '_none_') {
@@ -51,14 +178,13 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
             && $node->get('field_page_header_style')->value != 'standard') {
 
             // Page title.
-            $vmh_page_title = \Drupal::service('title_resolver')->getTitle(\Drupal::request(), \Drupal::routeMatch()->getRouteObject());
+            $vmh_page_title = $this->titleResolver->getTitle($this->requestStack->getCurrentRequest(), $this->routeMatch->getRouteObject());
             $vmh_page_title = is_array($vmh_page_title) ? $vmh_page_title['#markup'] : $vmh_page_title;
 
             // Page Breadcrumb block.
-            $block_manager = \Drupal::service('plugin.manager.block');
             $block_config = [];
-            $plugin_block = $block_manager->createInstance('system_breadcrumb_block', $block_config);
-            $access_result = $plugin_block->access(\Drupal::currentUser());
+            $plugin_block = $this->blockManager->createInstance('system_breadcrumb_block', $block_config);
+            $access_result = $plugin_block->access($this->currentUser);
             if (!(is_object($access_result)
                && $access_result->isForbidden()
                || is_bool($access_result)
@@ -69,7 +195,7 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
 
             $media_field_name = $config['vmh_node'][$node->bundle()];
 
-            $langcode = \Drupal::languageManager()->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)->getId();
+            $langcode = $this->languageManager->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)->getId();
 
             // Background media.
             $vmh_background_media = NULL;
@@ -85,8 +211,8 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
 
               if (!empty($node_field_media)) {
                 $node_field_media_entity = Media::load($node_field_media[0]['target_id']);
-                $node_field_media_build = \Drupal::entityTypeManager()->getViewBuilder('media')->view($node_field_media_entity, $config['vmh_media_view_mode']);
-                $vmh_background_media = \Drupal::service('renderer')->render($node_field_media_build);
+                $node_field_media_build = $this->entityTypeManager->getViewBuilder('media')->view($node_field_media_entity, $config['vmh_media_view_mode']);
+                $vmh_background_media = $this->renderer->render($node_field_media_build);
               }
             }
 
@@ -119,9 +245,10 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
     $form = parent::blockForm($form, $form_state);
     $config = $this->getConfiguration();
 
-    $vmh_settings = \Drupal::config('varbase_media_header.settings')->get(VARBASE_MEDIA_HEADER_SETTINGS);
+    $vmh_settings = $this->configFactory->config('varbase_media_header.settings')
+      ->get('varbase_media_header_settings');
 
-    $entity_info = \Drupal::entityManager()->getDefinitions();
+    $entity_info = $this->entityTypeManager()->getDefinitions();
 
     foreach ($entity_info as $entity_type_key => $entity_type) {
 
@@ -134,15 +261,15 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
           '#title' => $entity_type->getLabel(),
         ];
 
-        $bundles = \Drupal::entityManager()->getBundleInfo($entity_type_key);
+        $bundles = $this->entityTypeManager->getBundleInfo($entity_type_key);
         foreach ($bundles as $bundle_key => $bundle) {
           if (!empty($vmh_settings[$entity_type_key])
             && isset($vmh_settings[$entity_type_key][$bundle_key])
             && $vmh_settings[$entity_type_key][$bundle_key]) {
 
-            $options = ['_none_' => t("-  None  -")];
+            $options = ['_none_' => $this->t("-  None  -")];
 
-            $media_fields = \Drupal::service('entity_field.manager')->getFieldDefinitions($entity_type_key, $bundle_key);
+            $media_fields = $this->entityFieldManager->getFieldDefinitions($entity_type_key, $bundle_key);
             foreach ($media_fields as $field_name => $field_definition) {
               if (!empty($field_definition->getTargetBundle())) {
                 if ($field_definition->getType() == 'entity_reference'
@@ -152,15 +279,12 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
               }
             }
 
-            $bundle_label = \Drupal::entityTypeManager()
-              ->getStorage('node_type')
-              ->load($bundle_key)
-              ->label();
+            $bundle_label = $this->entityTypeManager->getStorage('node_type')->load($bundle_key)->label();
 
             $form['vmh_' . $entity_type_key][$bundle_key] = [
               '#type' => 'select',
               '#title' => $bundle_label,
-              '#description' => t('Choose a media field.'),
+              '#description' => $this->t('Choose a media field.'),
               '#options' => $options,
               '#default_value' => isset($config['vmh_' . $entity_type_key][$bundle_key]) ? $config['vmh_' . $entity_type_key][$bundle_key] : '_none_',
             ];
@@ -170,11 +294,11 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
       }
     }
 
-    $media_view_mode_options = \Drupal::service('entity_display.repository')->getViewModeOptions('media');
+    $media_view_mode_options = $this->entityDisplayRepository->getViewModeOptions('media');
     $form['vmh_media_view_mode'] = [
       '#type' => 'select',
-      '#title' => t('Media view mode'),
-      '#description' => t('Choose the Media view mode to use.'),
+      '#title' => $this->t('Media view mode'),
+      '#description' => $this->t('Choose the Media view mode to use.'),
       '#options' => $media_view_mode_options,
       '#default_value' => isset($config['vmh_media_view_mode']) ? $config['vmh_media_view_mode'] : '',
     ];
@@ -190,13 +314,15 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
     $values = $form_state->getValues();
 
     // Save configs for each entity type.
-    $vmh_settings = \Drupal::config('varbase_media_header.settings')->get(VARBASE_MEDIA_HEADER_SETTINGS);
-    $entity_info = \Drupal::entityManager()->getDefinitions();
+    $vmh_settings = $this->configFactory('varbase_media_header.settings')
+      ->get('varbase_media_header_settings');
+
+    $entity_info = $this->entityTypeManager->getDefinitions();
     foreach ($entity_info as $entity_type_key => $entity_type) {
       if (!empty($vmh_settings[$entity_type_key])
         && isset($vmh_settings[$entity_type_key])) {
 
-        $bundles = \Drupal::entityManager()->getBundleInfo($entity_type_key);
+        $bundles = $this->entityTypeManager->getBundleInfo($entity_type_key);
         foreach ($bundles as $bundle_key => $bundle) {
           if (!empty($vmh_settings[$entity_type_key])
             && isset($vmh_settings[$entity_type_key][$bundle_key])
@@ -216,13 +342,13 @@ class VarbaseMediaHeaderBlock extends BlockBase implements BlockPluginInterface 
    * {@inheritdoc}
    */
   public function getCacheTags() {
-    $node = \Drupal::routeMatch()->getParameter('node');
+    $node = $this->routeMatch->getParameter('node');
     if ($node instanceof NodeInterface) {
       if (isset($node)) {
         return Cache::mergeTags(parent::getCacheTags(), ['node:' . $node->id()]);
       }
     }
-    elseif(is_numeric($node)) {
+    elseif (is_numeric($node)) {
       return Cache::mergeTags(parent::getCacheTags(), ['node:' . (int) $node]);
     }
 
