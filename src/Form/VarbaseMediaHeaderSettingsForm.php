d5c15bdd8df1f78e67cfae35dcac7cde97fd4895
Qusai Taha <49427-qusaitaha@users.noreply.drupalcode.org>
2025-04-14 14:32:29 +0300

    Issue #3518954 by qusai taha: Fix ArgumentCountError in VarbaseMediaHeaderSettingsForm constructor

--- src/Form/VarbaseMediaHeaderSettingsForm.php
+++ src/Form/VarbaseMediaHeaderSettingsForm.php
@@ -14,6 +14,7 @@ use Drupal\Core\Url;
 use Vardot\Entity\EntityDefinitionUpdateManager;
 use Vardot\Installer\ModuleInstallerFactory;
 use Drupal\Core\DependencyInjection\ClassResolverInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
 
 /**
  * Varbase Media Header Settings Form Class.
@@ -49,10 +50,19 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
   protected $classResolver;
 
   /**
+   * The class resolver.
+   *
+   * @var \Drupal\Core\Config\TypedConfigManagerInterface
+   */
+  protected TypedConfigManagerInterface $typedConfigManager;
+
+  /**
    * Constructs a new Varbase Media Header Block.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The factory for configuration objects.
+   * @param \Drupal\Core\Config\TypedConfigManagerInterface $typedConfigManager
+   *   The typed config manager.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler service.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
@@ -62,8 +72,8 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
    * @param \Drupal\Core\DependencyInjection\ClassResolverInterface $class_resolver
    *   (optional) The class resolver.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, ModuleHandlerInterface $module_handler, EntityTypeManagerInterface $entity_type_manager, EntityTypeBundleInfoInterface $bundle_info, ClassResolverInterface $class_resolver) {
-    parent::__construct($config_factory);
+  public function __construct(ConfigFactoryInterface $config_factory, TypedConfigManagerInterface $typedConfigManager, ModuleHandlerInterface $module_handler, EntityTypeManagerInterface $entity_type_manager, EntityTypeBundleInfoInterface $bundle_info, ClassResolverInterface $class_resolver) {
+    parent::__construct($config_factory, $typedConfigManager);
     $this->moduleHandler = $module_handler;
     $this->entityTypeManager = $entity_type_manager;
     $this->bundleInfo = $bundle_info;
@@ -76,6 +86,7 @@ class VarbaseMediaHeaderSettingsForm extends ConfigFormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
+      $container->get('config.typed'),
       $container->get('module_handler'),
       $container->get('entity_type.manager'),
       $container->get('entity_type.bundle.info'),
